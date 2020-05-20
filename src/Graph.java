7813b8e106f8342473dce90c584ca026bd9dd68a
veeyuan <veeyuan0307@gmail.com>
2020-05-20 23:21:10 +0800

    2nd Commit: -implement locking -add callable class as thread -determine -add Graph class to store shared variables and methods

---
+++ src/Graph.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class Graph {
+    List<Point> pointList= new ArrayList<>();
+    List<Edge> edgeList=new ArrayList<>();
+    /* private static boolean gameOver = false;
+    */
+    private static boolean gameEnd = false; //win
+    static int matchedPointNum=0;
+
+    public Graph(){ }
+
+    public List<Point> getPointList(){
+        return  pointList;
+    }
+
+    public void setPointList(List<Point> pointList) {
+        this.pointList = pointList;
+    }
+
+    public int getPointListSize(){
+        return pointList.size();
+    }
+
+    public List<Edge> getEdgeList() {
+        return edgeList;
+    }
+
+    public Point createPoints(List<Point> pointList){
+        Random randGenerator = new Random();
+        double xCoor = randGenerator.nextDouble();
+        double yCoor = randGenerator.nextDouble();
+        Point newPoint = new Point(xCoor,yCoor);
+        //to avoid duplicate points are created - no two points are overlapped
+        if (pointList.contains(newPoint)){
+            return createPoints(pointList);
+        }
+        return  newPoint;
+    }
+
+    public List<Point> createPointList(List<Point> pointList, int PointNum) {
+        for (int i=0; i<PointNum; i++){
+            Point newPoint = createPoints(pointList);
+            pointList.add(newPoint);
+        }
+        return pointList;
+    }
+
+
+    public Point pickPoint () throws InterruptedException{
+
+        List<Point> pointList = getPointList();
+        Random randGenerator = new Random();
+        int pickPointIndex = randGenerator.nextInt(getPointListSize());
+        Point point = pointList.get(pickPointIndex);
+        boolean isSuccessfullyLocked = point.lock.tryLock();
+        if (!isSuccessfullyLocked){
+            System.out.println("Fails to lock -> pick another points");
+            return pickPoint();
+        }
+        return point;
+    }
+
+        public boolean match(Point a, Point b){
+            if (!a.isUsed() && !b.isUsed()){
+                a.setUsed(true);
+                b.setUsed(true);
+                //System.out.println("Point A ("+a.getxCoor()+","+a.getyCoor()+") and Point B ("+b.getxCoor()+","+b.getyCoor()+") are paired.");
+                matchedPointNum=matchedPointNum+2;
+                //System.out.println("Matched points>>>>>>"+matchedPointNum);
+                if (matchedPointNum>=pointList.size()){
+                    setGameEnd(true);
+                    System.out.println("-------------------------- All points are paired -------------------------");
+                }
+                a.lock.unlock();
+                b.lock.unlock();
+                return true;
+            }
+
+        a.lock.unlock();
+        b.lock.unlock();
+        return false;
+
+    }
+
+
+    public boolean isGameEnd() {
+        return gameEnd;
+    }
+
+    public void setGameEnd(boolean gameEnd) {
+        this.gameEnd = gameEnd;
+    }
+}
---
+++ src/PlayGame.java
@@ -0,0 +1,54 @@
+import java.util.concurrent.Callable;
+
+import static java.lang.Thread.sleep;
+
+public class PlayGame implements Callable {
+    Player player;
+    Graph graph;
+    Point A;
+    Point B;
+
+    public PlayGame(Player player, Graph graph) {
+        this.player = player;
+        this.graph = graph;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+
+        while (!graph.isGameEnd() ){
+            try {
+                if (player.getAttemptMatch()>20 ){
+                    System.out.println( "Player "+player.getPlayerId() +" stop matching >>> "+player.getEdgeList().size()+" matched...");
+                    return true;
+                }else{
+                    A = graph.pickPoint();
+                    B = graph.pickPoint();
+                    boolean success = graph.match(A,B);
+                    if (success){
+                        Edge playerEdge = new Edge(A,B);
+                        player.getEdgeList().add(playerEdge);
+/*                        Edge newEdge = new Edge(A,B,player.getPlayerId());
+                        graph.edgeList.add(newEdge);*/
+                        try {
+                            sleep(300);
+                        }catch (InterruptedException e){
+                            System.out.println("Interrupted >>>");
+                        }
+                        if (graph.isGameEnd()){
+                            return false;
+                        }
+                    }else{
+                        player.setAttemptMatch(player.getAttemptMatch()+1);
+                    }
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+
+}
