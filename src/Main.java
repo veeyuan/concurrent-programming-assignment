8ae4b26e93be2fe9b5e35327b151d9aad4d293b6
sngoh24 <47998988+sngoh24@users.noreply.github.com>
2020-05-20 21:20:27 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     //you may delete this
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+        //to accept input - number of threads
+        Scanner in = new Scanner(System.in);
+        System.out.print("Number of players you would like to create: ");
+        int playerNum = in.nextInt();
+        System.out.println("Number of players created :" + playerNum);
 
+        System.out.print("Number of points you would like to create: ");
+        int pointNum = in.nextInt();
+        while (pointNum <= playerNum) {
+            System.out.println("You should create more than thread number!");
+            System.out.print("Number of points you would like to create: ");
+            pointNum = in.nextInt();
+        }
+        System.out.println("Number of points created :" + pointNum);
+        System.out.println("-----------------------------------");
+
+        Game game = new Game(pointNum, playerNum);
+        game.startGame();
     }
 }
